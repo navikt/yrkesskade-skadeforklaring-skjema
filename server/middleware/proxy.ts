ba0436158a73c1c705f9dd1f3c4a3cae1a30af22
Jone Lura <jone.lura@gmail.com>
2022-04-06 16:20:31 +0200

    La på Nav-CallId på alle API requests

--- server/middleware/proxy.ts
+++ server/middleware/proxy.ts
@@ -1,6 +1,7 @@
 import { createProxyMiddleware, fixRequestBody } from 'http-proxy-middleware';
 import { exchangeToken } from '../tokenx';
 import { logError, logSecure, stdoutLogger } from '@navikt/yrkesskade-logging';
+import { v4 as uuidv4 } from 'uuid';
 
 const errorHandler = (err, req, res) => {
   if (process.env.ENV !== 'production') {
@@ -27,6 +28,8 @@ export const doProxy = (path: string, target: string) => {
       if (!tokenSet?.expired() && tokenSet?.access_token) {
         req.headers.authorization = `Bearer ${tokenSet.access_token}`;
       }
+
+      req.headers['Nav-CallId'] = uuidv4();
 
       return undefined;
     },
