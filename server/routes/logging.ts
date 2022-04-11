0075b2e3c8cbb51612d09b9d3f41ea5446cb6f22
Jone Lura <jone.lura@gmail.com>
2022-04-11 14:17:46 +0200

    La på session på logging

--- server/routes/logging.ts
+++ server/routes/logging.ts
@@ -1,6 +1,12 @@
-import { Express} from 'express';
+import { Express } from 'express';
 import { LogMessage } from '../../client/src/types/log-message';
-import { logInfo, logWarn, logError, logSecure, logDebug } from '@navikt/yrkesskade-logging';
+import {
+  logInfo,
+  logWarn,
+  logError,
+  logSecure,
+  logDebug,
+} from '@navikt/yrkesskade-logging';
 
 export const configureLoggingEndpoint = (app: Express): Express => {
   app.post(`/log`, handleLogging);
@@ -11,13 +17,21 @@ const handleLogging = (req, res) => {
   const logMessage = req.body as LogMessage;
 
   switch (logMessage.severity) {
-    case 'INFO' : logInfo(logMessage.message); break;
-    case 'WARN' : logWarn(logMessage.message); break;
-    case 'ERROR' : logError(logMessage.message); break;
-    case 'SECURE' : logSecure(logMessage.message); break;
-    default : logDebug(logMessage.message);
+    case 'INFO':
+      logInfo(logMessage.message, { session: logMessage.session });
+      break;
+    case 'WARN':
+      logWarn(logMessage.message, { session: logMessage.session });
+      break;
+    case 'ERROR':
+      logError(logMessage.message, null, { session: logMessage.session });
+      break;
+    case 'SECURE':
+      logSecure(logMessage.message, { session: logMessage.session });
+      break;
+    default:
+      logDebug(logMessage.message);
   }
 
   res.sendStatus(200);
-}
-
+};
